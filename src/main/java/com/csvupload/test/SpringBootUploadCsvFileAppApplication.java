4eae803477be20feaf0b83b6edd6b2618dddf983
LAPTOP-NMEUOAH3\HP <jatpariyakanish@gmail.com>
2023-08-06 23:05:11 +0530

    new project created

---
+++ src/main/java/com/csvupload/test/SpringBootUploadCsvFileAppApplication.java
@@ -0,0 +1,13 @@
+package com.csvupload.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadCsvFileAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadCsvFileAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csvupload/test/SpringBootUploadCsvFileAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csvupload.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadCsvFileAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
