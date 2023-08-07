807a2e6f1bf8b5270dd5245946174f6202b66edd
LAPTOP-NMEUOAH3\HP <jatpariyakanish@gmail.com>
2023-08-07 19:46:38 +0530

    Refactored code

--- src/main/resources/static/UploadPage/uploadcsv.js
+++ src/main/resources/static/UploadPage/uploadcsv.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var singleUploadForm = document.querySelector('#singleUploadForm');
+var singleFileUploadInput = document.querySelector('#singleFileUploadInput');
+var singleFileUploadError = document.querySelector('#singleFileUploadError');
+var singleFileUploadSuccess = document.querySelector('#singleFileUploadSuccess');
+var rowItem = document.querySelector('#studentdata')
+
 window.onload=init();
 
 function init() {
@@ -19,7 +25,11 @@ function loadUploadedFileData() {
 			if (response.length>0){
 				console.log("Data load success")
 				var output = response.map(i => "<tr><td>" + i.id + "</td><td>" + i.title + "</td><td>" + i.description + "</td><td>" + i.published + "</td></tr>");
-               $("#studentdata").html(output);
+				console.log(rowItem);
+				rowItem.innerHTML = output;
+//				document.getElementById("studentdata").appendChild(output)
+//               $("#studentdata").html(output);
+//				document.querySelector('#studentdata').innerHTML(output)
 			}
         } else {
           	console.log("Data not loaded successfull")
@@ -28,6 +38,43 @@ function loadUploadedFileData() {
 
     xhr.send();
 }
+
+function uploadSingleFile(file) {
+    var formData = new FormData();
+    formData.append("file", file);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/csv/upload");
+
+    xhr.onload = function() {
+        console.log(xhr.responseText);
+        var response = JSON.parse(xhr.responseText);
+        if(xhr.status == 200) {
+            singleFileUploadError.style.display = "none";
+            singleFileUploadSuccess.innerHTML = "<p>File Uploaded Successfully.</p><p>DownloadUrl : <a href='" + response.fileDownloadUri + "' target='_blank'>" + response.fileDownloadUri + "</a></p>";
+            singleFileUploadSuccess.style.display = "block";
+            loadUploadedFileData();
+        } else {
+            singleFileUploadSuccess.style.display = "none";
+            singleFileUploadError.innerHTML = "<p>"+(response && response.message)+"<p>" || "Some Error Occurred";
+            singleFileUploadSuccess.style.display = "block";
+
+        }
+    }
+
+    xhr.send(formData);
+}
+
+singleUploadForm.addEventListener('submit', function(event){
+    var files = singleFileUploadInput.files;
+    if(files.length === 0) {
+        singleFileUploadError.innerHTML = "Please select a file";
+        singleFileUploadError.style.display = "block";
+    }
+    uploadSingleFile(files[0]);
+    event.preventDefault();
+}, true);
+
 
 
 		
